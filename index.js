87b01b1a9c9e54df0124b32e8c60c21ebbd7f98c
Umair khan <umair.jadoon005@gmail.com>
2025-09-20 22:39:24 +0500

    static changes

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ const ADMIN_USER = {
 };
 app.use(expressLayouts);
 
-app.use(express.static('uploads'));
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/uploads'));
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
